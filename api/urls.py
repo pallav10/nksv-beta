d6d1b3e961647a99192763473f73f466f6af7494
Pallav <pallav8parikh@gmail.com>
2017-07-16 19:17:21 +0530

    fixing urls

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,6 @@ import views
 
 
 urlpatterns = [
-    url(r'^$', views.user_registration),
     url(r'^users/$', views.user_registration),
     url(r'^users/(?P<pk>[0-9]+)/$', views.user_detail),
     url(r'^users/login/$', views.user_login),

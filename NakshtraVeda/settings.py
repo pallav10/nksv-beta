d1dcc6d92807981ee2ad5906e3cf9aa0d1a1de0b
Pallav <pallav8parikh@gmail.com>
2017-07-16 19:07:52 +0530

    staticfiles and runtime.txt added

--- NakshtraVeda/settings.py
+++ NakshtraVeda/settings.py
@@ -163,11 +163,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'api/staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'api/staticfiles')
+# # Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'api/static'),
+# )
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'api/static'),
+    os.path.join(BASE_DIR, 'static'),
 )
+
+
 # To explicitly define the user authentication model.
 AUTH_USER_MODEL = 'api.User'
 

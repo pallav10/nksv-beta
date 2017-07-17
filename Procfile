bfda61a6a92bab815c73bb7a0be7da84c9b8abaf
Pallav <pallav8parikh@gmail.com>
2017-07-17 10:51:40 +0530

    reverting back

--- Procfile
+++ Procfile
@@ -2,4 +2,3 @@ web: python manage.py collectstatic --noinput
 web: python manage.py makemigrations
 web: python manage.py migrate
 web: gunicorn NakshtraVeda.wsgi
-web: open ./index.html
--- js/nocart.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-		$("#guest-cart").css("cursor", "default");
-    $('#guest-cart').on("click", function(e) {
-        e.preventDefault();
-        $("#guest-cart-message").text("Please login to view cart");
-    });
-});
